ebae54004ccc621f5bcfc7dffa9e1fc2a1542f10
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2025-12-29 23:22:48 -0300

    add player to server host and disconnect player function

--- High_Level_Multiplayer/Scripts/high_level_network_handler.gd
+++ High_Level_Multiplayer/Scripts/high_level_network_handler.gd
@@ -1,6 +1,6 @@
 extends Node
 
-#signal host_started()
+signal host_started()
 
 const IP_ADRESS: String = "localhost"
 const PORT: int = 42069
@@ -8,13 +8,17 @@ const MAX_CLIENTS = 4 # Default = 32
 
 var peer: ENetMultiplayerPeer
 
+var players = {}
+var player_info = {"name": "Name"}
+var players_loaded = 0
+
 # ---------------------------------------------------------------------------- #
 
 func start_server() -> void:
 	peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT, MAX_CLIENTS)
 	multiplayer.multiplayer_peer = peer
-	#host_started.emit()
+	host_started.emit()
 	
 # ---------------------------------------------------------------------------- #
 
--- High_Level_Multiplayer/Scripts/player_spawner.gd
+++ High_Level_Multiplayer/Scripts/player_spawner.gd
@@ -6,22 +6,29 @@ extends MultiplayerSpawner
 
 func _ready() -> void:
 	multiplayer.peer_connected.connect(spawn_player)
-	#HighLevelNetworkHandler.host_started.connect(spawn_host_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	
+	if multiplayer.is_server():
+		spawn_player(1) # 1 is always the host id
 
 # ---------------------------------------------------------------------------- #
 	
 func spawn_player(id: int) -> void:
-	# Execute Server Only
-	if multiplayer.is_server():
-		var player : Node = playerScene.instantiate()
-		player.name = str(id)
-		get_node(spawn_path).call_deferred("add_child", player)
+	
+	if not multiplayer.is_server(): return
+	
+	var player : Node = playerScene.instantiate()
+	player.name = str(id)
+	get_node(spawn_path).call_deferred("add_child", player) # add to the parent node
 
 # ---------------------------------------------------------------------------- #
 
-func spawn_host_player() -> void:
-	# Execute Server Only
+func remove_player(id: int) -> void:
+	
 	if not multiplayer.is_server(): return
-	spawn_player(multiplayer.get_unique_id())
-
+		
+	var path_node = get_node(spawn_path)
+	if path_node.has_node(str(id)):
+		path_node.get_node(str(id)).queue_free()
+		
 # ---------------------------------------------------------------------------- #
