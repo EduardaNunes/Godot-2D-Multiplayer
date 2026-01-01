21812eff69e45ca7894a91db51d81c0616da69e7
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-01-01 18:35:27 -0300

    Create esc menu scene and add it to player (has bugs)

--- High_Level_Multiplayer/Scripts/high_level_network_handler.gd
+++ High_Level_Multiplayer/Scripts/high_level_network_handler.gd
@@ -6,7 +6,8 @@ const IP_ADRESS: String = "localhost"
 const PORT: int = 42069
 const MAX_CLIENTS = 4 # Default = 32
 
-var peer: ENetMultiplayerPeer
+var client: ENetMultiplayerPeer
+var server: ENetMultiplayerPeer
 
 var players = {}
 var player_info = {"name": "Name"}
@@ -15,16 +16,16 @@ var players_loaded = 0
 # ---------------------------------------------------------------------------- #
 
 func start_server() -> void:
-	peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT, MAX_CLIENTS)
-	multiplayer.multiplayer_peer = peer
+	server = ENetMultiplayerPeer.new()
+	server.create_server(PORT, MAX_CLIENTS)
+	multiplayer.multiplayer_peer = server
 	host_started.emit()
 	
 # ---------------------------------------------------------------------------- #
 
 func start_client() -> void:
-	peer = ENetMultiplayerPeer.new()
-	peer.create_client(IP_ADRESS, PORT)
-	multiplayer.multiplayer_peer = peer
+	client = ENetMultiplayerPeer.new()
+	client.create_client(IP_ADRESS, PORT)
+	multiplayer.multiplayer_peer = client
 
 # ---------------------------------------------------------------------------- #
