12dbb5d2e399d87bd5aadefc101aa3c8d5b37d5d
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-01-02 03:54:52 -0300

    change mouse aim sprite and lifes border sprite to white

--- High_Level_Multiplayer/Scripts/mouse_aim.gd
+++ High_Level_Multiplayer/Scripts/mouse_aim.gd
@@ -1,5 +1,12 @@
 extends Sprite2D
 
+@onready var Player : CharacterBody2D = $".."
+
+# ---------------------------------------------------------------------------- #
+
 func _physics_process(delta: float) -> void:
-	#position = get_global_mouse_position()
-	look_at(get_global_mouse_position())
+	global_position = get_global_mouse_position()
+	look_at(Player.position)
+	rotation += PI
+
+# ---------------------------------------------------------------------------- #
