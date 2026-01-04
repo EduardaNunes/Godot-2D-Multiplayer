2d5082717b889f93a1c907506b710267b032679c
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-01-04 05:48:24 -0300

    fix players sound effects + fix sounds and background music volumes

--- High_Level_Multiplayer/Scripts/player_stream_sounds.gd
+++ High_Level_Multiplayer/Scripts/player_stream_sounds.gd
@@ -1,3 +1,5 @@
+# ---------------------------------------------------------------------------- #
+
 extends AudioStreamPlayer2D
 
 @onready var player : CharacterBody2D = $".."
@@ -6,19 +8,32 @@ const SOUND_TROW = preload("res://High_Level_Multiplayer/Assets/Sounds/shoot1.wa
 const SOUND_HURT = preload("res://High_Level_Multiplayer/Assets/Sounds/SFX Error.wav")
 const SOUND_DEATH = preload("res://High_Level_Multiplayer/Assets/Sounds/explode04.wav")
 
+# ---------------------------------------------------------------------------- #
+
 func _ready() -> void:
 	HighLevelNetworkHandler.player_trow_snowball.connect(func(a,b,c): play_trow_sound())
-	player.took_damage.connect(func(a): play_hurt_sound())
+	player.took_damage.connect(func(lifes): play_hurt_sound(lifes))
 	player.player_died.connect(func(): play_death_sound())
 	
+# ---------------------------------------------------------------------------- #
+
 func play_trow_sound():
 	stream = SOUND_TROW
 	play()
 
-func play_hurt_sound():
+# ---------------------------------------------------------------------------- #
+
+func play_hurt_sound(player_lifes):
+	if player_lifes <= 0: return
+	print("hurt")
 	stream = SOUND_HURT
 	play()
 	
+# ---------------------------------------------------------------------------- #
+
 func play_death_sound():
-	stream = SOUND_HURT
+	print("death")
+	stream = SOUND_DEATH
 	play()
+
+# ---------------------------------------------------------------------------- #
