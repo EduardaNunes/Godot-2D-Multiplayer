75d485c6e14f7723dfabd3d001dd1ffb0d239618
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-01-04 02:54:28 -0300

    add global background music

---
+++ High_Level_Multiplayer/Scenes/background_music.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@onready var musicPlayer = $AudioPlayer
+
+func play_music(newMusic: AudioStream):
+	if musicPlayer.stream == newMusic and musicPlayer.playing:
+		return
+	
+	musicPlayer.stream = newMusic
+	musicPlayer.play()
+
+func stop_music():
+	musicPlayer.stop()
